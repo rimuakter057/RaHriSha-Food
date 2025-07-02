876acad97ae589244cd85c4d741efbf0aef9413d
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-07-02 19:31:29 +0600

    add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async{
+  await Supabase.initialize(
+      url: "https://szylekgapqhyqynnnodu.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6eWxla2dhcHFoeXF5bm5ub2R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0Mzk1NDIsImV4cCI6MjA2NzAxNTU0Mn0.Er7oIPUYI5hrxlsUylC6N4ezTYth0wzjNwmjOQi4fGU"
+  );
   runApp(const RaHriShaFood());
 }
 
