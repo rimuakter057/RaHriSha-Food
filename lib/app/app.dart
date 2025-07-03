fad1f7548b13239aa48a045e636b3778bd0b9a50
ranasheikh6424 <rana6424sheikh@gmail.com>
2025-07-03 01:16:01 -0700

    complete, recipe_details.dart,recipe_post,editrecipe-page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:rahrisha_food/features/home/screens/home_screen.dart';
 
 class RaHriShaFood extends StatefulWidget {
   const RaHriShaFood({super.key});
@@ -11,26 +13,36 @@ class RaHriShaFood extends StatefulWidget {
 class _RaHriShaFoodState extends State<RaHriShaFood> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: Colors.grey.shade100)
-          )
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            fixedSize: Size(double.infinity, 50),
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.deepOrange,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12)
-            )
-          )
-        )
-      ),
-      //home: RecipeDetailPage(),
+    return ScreenUtilInit(
+        designSize: const Size(393, 852),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'RaHriShaFood',
+            theme: ThemeData(
+                inputDecorationTheme: InputDecorationTheme(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(color: Colors.grey.shade100)
+                    )
+                ),
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: Size(double.infinity, 50),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.deepOrange,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)
+                        )
+                    )
+                )
+            ),
+            //home: RecipeDetailPage(),
+            home: HomeScreen(),
+          );
+        }
     );
   }
 }
