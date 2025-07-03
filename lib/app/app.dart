99963bd930e5b157e7ac6bb7edea55226c36cfb0
ranasheikh6424 <rana6424sheikh@gmail.com>
2025-07-03 00:37:45 -0700

    complete, recipe_details.dart,recipe_post,edit,recipe

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rahrisha_food/features/recepie/screens/Edit_recipe.dart';
+import 'package:rahrisha_food/features/recepie/screens/recipe_details.dart';
+import 'package:rahrisha_food/features/recepie/screens/upload_recipe.dart';
 
 
 class RaHriShaFood extends StatefulWidget {
@@ -12,7 +15,27 @@ class RaHriShaFood extends StatefulWidget {
 class _RaHriShaFoodState extends State<RaHriShaFood> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: Colors.grey.shade100)
+          )
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(double.infinity, 50),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepOrange,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12)
+            )
+          )
+        )
+      ),
+      //home: RecipeDetailPage(),
+    );
   }
 }
 
