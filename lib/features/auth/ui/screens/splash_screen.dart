bbd080c9b95ebd22aad54e76fa07c2053f7c0f2e
Mardeya <shamim.parvez.cad@gmail.com>
2025-07-04 20:08:40 +0600

    splash screen and home carosel update

---
+++ lib/features/auth/ui/screens/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:rahrisha_food/app/app_logo.dart';
+import 'package:rahrisha_food/app/app_text.dart';
+import 'package:rahrisha_food/features/home/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              const AppLogo(),
+              const Spacer(),
+              SizedBox(height: 16),
+              Text(
+                AppText.foodSwift,
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+              ),
+              SizedBox(height: 16),
+              Text(
+                AppText.deliciousFood,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+              const Spacer(),
+              const CircularProgressIndicator(),
+              const SizedBox(height: 16),
+              Text(AppText.version),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
