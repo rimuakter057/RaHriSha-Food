1244e822d45cc79bc103907a0874a24e162afa62
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-07-05 12:57:53 +0600

    app file

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:rahrisha_food/features/blog/screen/blog_screen.dart';
-import 'package:rahrisha_food/features/recepie/screens/Edit_recipe.dart';
-import 'package:rahrisha_food/features/recepie/screens/recipe_details.dart';
-import 'package:rahrisha_food/features/recepie/screens/upload_recipe.dart';
-import 'package:rahrisha_food/features/serch/screens/search_screen.dart';
-import 'package:rahrisha_food/features/wishlist/screen/wishlist_screen.dart';
-
 import '../features/common/ui/screens/main_bottom_nav_screen.dart';
 
 class RaHriShaFood extends StatefulWidget {
